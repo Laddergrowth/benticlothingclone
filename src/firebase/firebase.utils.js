2d8af231d27db4ba1148c74751de2ad3f91999dd
lucas <info.mildp@gmail.com>
2020-01-30 20:32:36 +0000

    Implemented sign-in-and-sgn-up page working with firebase- with google sign in and email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,35 @@ const config = {
     measurementId: "G-G8JLG8FK40"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+           await userRef.set({
+            displayName,
+            email,
+            createAt,
+            ...additionalData
+
+           })
+        } catch (error) {
+          console.log('error creating user', error.message);
+
+        }
+    }
+
+
+  return userRef;
+  };
+
 
 firebase.initializeApp(config);
 
